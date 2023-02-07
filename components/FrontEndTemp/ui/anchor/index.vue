d30a8046d5533d81a95cf2a858439fc610dae980
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2023-02-07 23:04:38 +0530

    New Folders Added from the React Project

---
+++ components/FrontEndTemp/ui/anchor/index.vue
@@ -0,0 +1,84 @@
+<template>
+  if (!path) return null; const internal = /^\/(?!\/)/.test(path); if
+  (!internal) { const isHash = path.startsWith("#"); if (isHash) { return (
+  <a
+    rel="{rel}"
+    className="{className}"
+    href="{path}"
+    onClick="{onClick}"
+    onKeyPress="{onKeyPress}"
+    onFocus="{onFocus}"
+    onBlur="{onBlur}"
+    {...rest}
+  >
+    {children}
+  </a>
+  ); }
+</template>
+
+<script>
+// import { ReactNode } from "react";
+// import Link from "next/link"; // use NuxtLink here
+
+defineProps({
+  target: "_blank",
+  rel: "noopener noreferrer",
+});
+
+type TProps = typeof defaultProps &
+  HTMLDivElement.HTMLAttributes<HTMLAnchorElement> & {
+    path: string,
+    children: ReactNode,
+    className?: string,
+    onClick?: () => void,
+    onKeyPress?: (e: React.KeyboardEvent<HTMLAnchorElement>) => void,
+    onFocus?: (e: React.FocusEvent<HTMLAnchorElement>) => void,
+    onBlur?: (e: React.FocusEvent<HTMLAnchorElement>) => void,
+  };
+</script>
+
+const Anchor = ({ path, children, className, rel, target, onClick, onKeyPress,
+onFocus, onBlur, ...rest }: TProps) => { if (!path) return null; const internal
+= /^\/(?!\/)/.test(path); if (!internal) { const isHash = path.startsWith("#");
+if (isHash) { return (
+<a
+  rel="{rel}"
+  className="{className}"
+  href="{path}"
+  onClick="{onClick}"
+  onKeyPress="{onKeyPress}"
+  onFocus="{onFocus}"
+  onBlur="{onBlur}"
+  {...rest}
+>
+                    {children}
+                </a>
+); } return (
+<a
+  rel="{rel}"
+  className="{className}"
+  href="{path}"
+  target="{target}"
+  onClick="{onClick}"
+  onKeyPress="{onKeyPress}"
+  onFocus="{onFocus}"
+  onBlur="{onBlur}"
+  {...rest}
+>
+                {children}
+            </a>
+); } return (
+<Link href="{path}" passHref>
+            <a
+                className={className}
+                onClick={onClick}
+                onKeyPress={onKeyPress}
+                onFocus={onFocus}
+                onBlur={onBlur}
+                {...rest}
+            >
+                {children}
+            </a>
+        </Link>
+); }; Anchor.defaultProps = defaultProps; Anchor.displayName = "Anchor"; export
+default Anchor;
